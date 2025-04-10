1efb4a19698032a6ddb052f53a048dea0c647052
Jaebeom Lee <8000gam@gmail.com>
2025-04-10 11:47:55 +0900

    Revise main script L 홈 화면 리디자인 L 구분별 분류에 총 개수 구현 L 이미지 to URL 버튼 샘플 구현: 37번 라인 참조.

--- sample_page_1.py
+++ sample_page_1.py
@@ -10,7 +10,7 @@ if "history" not in st.session_state:
 # 2. 필요한 데이터 집계
 
 # 데이터프레임 불러오기
-df = pd.read_csv("insurance_info.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/SKNETWORKS-FAMILY-AICAMP/SKN13-1st-4Team/refs/heads/database/DB/car_ins.csv")
 
 # 할인율 범위로 보여줘야 되니까 최댓값 최솟값 모으기기
 min_category = []
@@ -34,18 +34,53 @@ def go_back()   -> None:
     if st.session_state.current_page != "home":
         st.session_state.current_page = st.session_state.history.pop()    
 
-
+# 함수: 이미지 to URL 버튼 구현 $$$샘플입니다.$$$
+# 일단 이미지는 삼성 로고랑, ULR은 네이버 도메인으로 임의로 설정했어요. 기호에 맞게 사용하면 되려나..?
+def samsung_button():
+    st.markdown("""
+        <a href="https://www.naver.com" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/512px-Samsung_Logo.svg.png"
+                 alt="삼성화재"
+                 style="width:200px; height:auto; margin: 10px 0;">
+        </a>
+    """, unsafe_allow_html=True)
 
 # 함수: 페이지 구현
 
 # 홈
 def home():
-    st.title("자동자보험정보시스템")
+    st.title("자동차보험특약정보시스템")
+    st.subheader("여러 회사의 자동차 보험 특별 약관을 종류별로 보여줍니다.")
+
+    st.markdown("""
+        <style>
+        div[data-testid="stButton"] > button {
+            width: 300px;
+            height: 60px;
+            font-size: 30px;
+            font-weight: bold;
+            border-radius: 12px;
+            background-color: #FFFFFF;
+            color: black;
+            margin: 10px 0;
+        }
+
+        div[data-testid="stButton"] > button:hover {
+            background-color: #f0f0f0;
+            color: black;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
     left_col, right_col = st.columns(2)
     with left_col:
         st.button("구분별 조회", on_click = lambda: go_to("구분별 페이지"))
+        st.caption("예) 자녀할인, 블랙박스장착할인")
     with right_col:
         st.button("보험사별 조회", on_click = lambda: go_to("보험사별 페이지"))
+        st.caption("예) DB손해보험, 삼성화재")
+
+    samsung_button()
 
 # 보험사선택: 
 def select_company():
@@ -67,37 +102,98 @@ def select_condition():
     st.button(f"서민(나눔)우대할인: {min_category[5]}% ~ {max_category[5]}%", 
               on_click = lambda: go_to("서민(나눔)우대할인 페이지"))
 
+
 # 구분별: 6개
+
 def child_dc():
-    st.title("자녀할인")
-    st.write(f"할인율: {min_category[0]}% ~ {max_category[0]}%")
-    for i in df['회사명'].unique():
-        st.button(i, on_click = lambda i=i: go_to(f"자녀할인_{i} 페이지"))
+    st.title(f"자녀할인: 총 {len(df[df['구분']=='자녀할인'])}개")
+    st.subheader(f"할인율: {min_category[0]}% ~ {max_category[0]}%")
+
+    right_col, center_col, left_col = st.columns(3)
+    with right_col:
+        for i in df['회사명'].unique()[:4]:
+            st.button(i, on_click = lambda i=i: go_to(f"자녀할인_{i} 페이지"))
+    with center_col:
+        for i in df['회사명'].unique()[4:8]:
+            st.button(i, on_click = lambda i=i: go_to(f"자녀할인_{i} 페이지"))
+    with left_col:
+        for i in df['회사명'].unique()[8:]:
+            st.button(i, on_click = lambda i=i: go_to(f"자녀할인_{i} 페이지"))
+
 def blackbox_dc():
-    st.title("블랙박스장착할인")
-    st.write(f"할인율: {min_category[1]}% ~ {max_category[1]}%")
-    for i in df['회사명'].unique():
-        st.button(i, on_click = lambda i=i: go_to(f"블랙박스장착할인_{i} 페이지"))
+    st.title(f"블랙박스장치할인: 총 {len(df[df['구분']=='블랙박스장착할인'])}개")
+    st.subheader(f"할인율: {min_category[1]}% ~ {max_category[1]}%")
+ 
+    right_col, center_col, left_col = st.columns(3)
+    with right_col:
+        for i in df['회사명'].unique()[:4]:
+            st.button(i, on_click = lambda i=i: go_to(f"블랙박스장착할인_{i} 페이지"))
+    with center_col:
+        for i in df['회사명'].unique()[4:8]:
+            st.button(i, on_click = lambda i=i: go_to(f"블랙박스장착할인_{i} 페이지"))
+    with left_col:
+        for i in df['회사명'].unique()[8:]:
+            st.button(i, on_click = lambda i=i: go_to(f"블랙박스장착할인_{i} 페이지"))
+
 def mileage_dc():
-    st.title("마일리지(후정산형)할인")
-    st.write(f"할인율: {min_category[2]}% ~ {max_category[2]}%")
-    for i in df['회사명'].unique():
-        st.button(i, on_click = lambda i=i: go_to(f"마일리지(후정산형)할인_{i} 페이지"))
+    st.title(f"마일리지(후정산형)할인: 총 {len(df[df['구분']=='마일리지(후정산형)할인'])}개")
+    st.subheader(f"할인율: {min_category[2]}% ~ {max_category[2]}%")
+
+    right_col, center_col, left_col = st.columns(3)
+    with right_col:
+        for i in df['회사명'].unique()[:4]:
+            st.button(i, on_click = lambda i=i: go_to(f"마일리지(후정산형)할인_{i} 페이지"))
+    with center_col:
+        for i in df['회사명'].unique()[4:8]:
+            st.button(i, on_click = lambda i=i: go_to(f"마일리지(후정산형)할인_{i} 페이지"))
+    with left_col:
+        for i in df['회사명'].unique()[8:]:
+            st.button(i, on_click = lambda i=i: go_to(f"마일리지(후정산형)할인_{i} 페이지"))
+
 def ldws_dc():
-    st.title("차선이탈경고(방지)장치할인")
-    st.write(f"할인율: {min_category[3]}% ~ {max_category[3]}%")
-    for i in df['회사명'].unique():
-        st.button(i, on_click = lambda i=i: go_to(f"차선이탈경고(방지)장치할인_{i} 페이지"))
+    st.title(f"차선이탈경고(방지)장치할인: 총 {len(df[df['구분']=='차선이탈경고(방지)장치할인'])}개")
+    st.subheader(f"할인율: {min_category[3]}% ~ {max_category[3]}%")
+
+    right_col, center_col, left_col = st.columns(3)
+    with right_col:
+        for i in df['회사명'].unique()[:4]:
+            st.button(i, on_click = lambda i=i: go_to(f"차선이탈경고(방지)장치할인_{i} 페이지"))
+    with center_col:
+        for i in df['회사명'].unique()[4:8]:
+            st.button(i, on_click = lambda i=i: go_to(f"차선이탈경고(방지)장치할인_{i} 페이지"))
+    with left_col:
+        for i in df['회사명'].unique()[8:]:
+            st.button(i, on_click = lambda i=i: go_to(f"차선이탈경고(방지)장치할인_{i} 페이지"))
+
 def elderly_lecture_dc():
-    st.title("고령자안전교육이수할인")
-    st.write(f"할인율: {min_category[4]}% ~ {max_category[4]}%")
-    for i in df['회사명'].unique():
-        st.button(i, on_click = lambda i=i: go_to(f"고령자안전교육이수할인_{i} 페이지"))
+    st.title(f"고령자안전교육이수할인: 총 {len(df[df['구분']=='고령자안전교육이수할인'])}")
+    st.subheader(f"할인율: {min_category[4]}% ~ {max_category[4]}%")
+
+    right_col, center_col, left_col = st.columns(3)
+    with right_col:
+        for i in df['회사명'].unique()[:4]:
+            st.button(i, on_click = lambda i=i: go_to(f"고령자안전교육이수할인_{i} 페이지"))
+    with center_col:
+        for i in df['회사명'].unique()[4:8]:
+            st.button(i, on_click = lambda i=i: go_to(f"고령자안전교육이수할인_{i} 페이지"))
+    with left_col:
+        for i in df['회사명'].unique()[8:]:
+            st.button(i, on_click = lambda i=i: go_to(f"고령자안전교육이수할인_{i} 페이지"))
+
 def commoner_dc():
-    st.title("서민(나눔)우대할인")
-    st.write(f"할인율: {min_category[5]}% ~ {max_category[5]}%")
-    for i in df['회사명'].unique():
-        st.button(i, on_click = lambda i=i: go_to(f"서민(나눔)우대할인_{i} 페이지"))
+    st.title(f"서민(나눔)우대할인: 총 {len(df[df['구분']=='서민(나눔)우대할인'])}")
+    st.subheader(f"할인율: {min_category[5]}% ~ {max_category[5]}%")
+
+    right_col, center_col, left_col = st.columns(3)
+    with right_col:
+        for i in df['회사명'].unique()[:4]:
+            st.button(i, on_click = lambda i=i: go_to(f"서민(나눔)우대할인_{i} 페이지"))
+    with center_col:
+        for i in df['회사명'].unique()[4:8]:
+            st.button(i, on_click = lambda i=i: go_to(f"서민(나눔)우대할인_{i} 페이지"))
+    with left_col:
+        for i in df['회사명'].unique()[8:]:
+            st.button(i, on_click = lambda i=i: go_to(f"서민(나눔)우대할인_{i} 페이지"))
 
 # 이제 6 * 12 = 72개의 하위 페이지 함수를 만들면 되겠군요....!
 def dynamic_detail_page():
@@ -121,7 +217,7 @@ def dynamic_detail_page():
 
 # 뒤로가기는 홈화면에선 보여주지 마세요.
 if st.session_state.current_page != "home":
-    st.button("⬅", on_click=go_back)
+    st.button("⬅️ 뒤로가기", on_click=go_back)
 
 # 딕셔너리에 페이지와 함수 매핑
 pages = {
@@ -131,7 +227,7 @@ pages = {
     "자녀할인 페이지": child_dc,
     "블랙박스장착할인 페이지": blackbox_dc,
     "마일리지(후정산형)할인 페이지": mileage_dc,
-    '차선이탈경고(방지)장치할인 페이지':  ldws_dc, ## lane departure warning system
+    '차선이탈경고(방지)장치할인 페이지': ldws_dc, ## lane departure warning system
     '고령자안전교육이수할인 페이지': elderly_lecture_dc,
     '서민(나눔)우대할인 페이지': commoner_dc
 }
@@ -145,3 +241,4 @@ else:
 # 그냥 커스텀어트리뷰트 current_page랑 history 실시간 조회.
 st.write(f"현재 페이지: {st.session_state.current_page}")
 st.write(f"페이지 로그 스택 방식으로 저장한 거 보여줄게요: {st.session_state.history}")
+
