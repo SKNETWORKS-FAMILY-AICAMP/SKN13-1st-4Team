95b718b733ba7b8e500fe2bff7468f56ef48a153
Park Hyuna <hyunah2674@naver.com>
2025-04-10 09:41:11 +0900

    mysql 없는 버전

--- Chy_Project_박현아.py
+++ Chy_Project_박현아.py
@@ -25,6 +25,7 @@ def go_back()   -> None:
 
 # 함수: 페이지 구현
 
+    
 # 홈
 def home():
     st.title("자동자보험정보시스템")
@@ -61,30 +62,31 @@ def select_company():
     with col3:
         st.button("MG손해보험", on_click = lambda: go_to("MG손보 페이지"))
         st.button("흥국화재해상", on_click = lambda: go_to("흥국화재 페이지"))
-        st.button("하나손해보험", on_click = lambda: go_to("하나손해보험 페이지"))
+        # st.button("하나손해보험", on_click = lambda: go_to("하나손해보험 페이지"))
         st.button("캐롯손해보험", on_click = lambda: go_to("캐롯손해보험 페이지"))
 
 
-# 보험사 detail 파일 불러오기
-import pymysql
-import pandas as pd
+# # 보험사 detail 파일 불러오기
+# import pymysql
+# import pandas as pd
 
-# DB 연결
-conn = pymysql.connect(
-    host='127.0.0.1',          # 또는 DB 주소
-    user='SKN13',
-    password='1111',
-    database='car_insurance',
-    charset='utf8mb4',         # 한글 깨짐 방지
-   # cursorclass=pymysql.cursors.DictCursor  # 딕셔너리 형태로 가져오기
-)
+# # DB 연결
+# conn = pymysql.connect(
+#     host='127.0.0.1',          # 또는 DB 주소
+#     user='SKN13',
+#     password='1111',
+#     database='car_insurance',
+#     charset='utf8mb4',         # 한글 깨짐 방지
+#    # cursorclass=pymysql.cursors.DictCursor  # 딕셔너리 형태로 가져오기
+# )
 
-# 쿼리 작성
-query = "SELECT * FROM car_number"
+# # 쿼리 작성
+# query = "SELECT * FROM car_number"
 
-# pandas로 DataFrame 불러오기
-insurance_detail = pd.read_sql(query, conn)
+# # pandas로 DataFrame 불러오기
+# insurance_detail = pd.read_sql(query, conn)
 
+insurance_detail = pd.read_csv("보험사_url_전화번호.csv")
 df = pd.read_csv("car_ins.csv")
 
 # 대표번호 불러오기
@@ -108,10 +110,13 @@ def samsung():
     st.title("삼성화재")
     st.subheader("대표번호")
     st.text(number(5))
-    st.subheader("상담센터")
-    st.link_button(consult(5), consult(5))
-    st.subheader("민원창구")
-    st.link_button(complain(5), complain(5))
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(5))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(5))
+    st.subheader("특약 종류")
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"삼성화재_{i} 페이지"))
 
@@ -121,10 +126,13 @@ def hyundai():
     st.title("현대해상")
     st.subheader("대표번호")
     st.text(number(6))
-    st.subheader("상담센터")
-    st.link_button(consult(6), consult(6))
-    st.subheader("민원창구")
-    st.link_button(complain(6), complain(6)) 
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(6))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(6))
+    st.subheader("특약 종류") 
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"현대해상_{i} 페이지"))
 
@@ -133,10 +141,13 @@ def db_insu():
     st.title("DB손해보험")
     st.subheader("대표번호")
     st.text(number(8))
-    st.subheader("상담센터")
-    st.link_button(consult(8), consult(8))
-    st.subheader("민원창구")
-    st.link_button(complain(8), complain(8))
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(8))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(8))
+    st.subheader("특약 종류")
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"DB손보_{i} 페이지"))
 
@@ -145,10 +156,13 @@ def kb_insu():
     st.title("KB손해보험")
     st.subheader("대표번호")
     st.text(number(7))
-    st.subheader("상담센터")
-    st.link_button(consult(7), consult(7))
-    st.subheader("민원창구")
-    st.link_button(complain(7), complain(7))
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(7))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(7))
+    st.subheader("특약 종류")
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"KB손보_{i} 페이지"))
 
@@ -157,10 +171,13 @@ def meritz():
     st.title("메리츠화재보험")
     st.subheader("대표번호")
     st.text(number(0))
-    st.subheader("상담센터")
-    st.link_button(consult(0), consult(0))
-    st.subheader("민원창구")
-    st.link_button(complain(0), complain(0))
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(0))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(0))
+    st.subheader("특약 종류")
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"메리츠화재_{i} 페이지"))
 
@@ -169,10 +186,13 @@ def AXA():
     st.title("AXA손해보험")
     st.subheader("대표번호")
     st.text(number(9))
-    st.subheader("상담센터")
-    st.link_button(consult(9), consult(9))
-    st.subheader("민원창구")
-    st.link_button(complain(9), complain(9))
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(9))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(9))
+    st.subheader("특약 종류")
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"AXA손보_{i} 페이지"))
 
@@ -181,10 +201,13 @@ def hanhwa():
     st.title("한화손해보험")
     st.subheader("대표번호")
     st.text(number(1))
-    st.subheader("상담센터")
-    st.link_button(consult(1), consult(1))
-    st.subheader("민원창구")
-    st.link_button(complain(1), complain(1))
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(1))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(1))
+    st.subheader("특약 종류")
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"한화손보_{i} 페이지"))
 
@@ -193,10 +216,13 @@ def lotte():
     st.title("롯데손해보험")
     st.subheader("대표번호")
     st.text(number(2))
-    st.subheader("상담센터")
-    st.link_button(consult(2), consult(2))
-    st.subheader("민원창구")
-    st.link_button(complain(2), complain(2)) 
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(2))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(2))
+    st.subheader("특약 종류") 
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"롯데손보_{i} 페이지"))
         
@@ -205,10 +231,13 @@ def MG():
     st.title("MG손해보험")
     st.subheader("대표번호")
     st.text(number(3))
-    st.subheader("상담센터")
-    st.link_button(consult(3), consult(3))
-    st.subheader("민원창구")
-    st.link_button(complain(3), complain(3)) 
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(3))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(3))
+    st.subheader("특약 종류") 
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"MG손보_{i} 페이지"))
 
@@ -216,35 +245,41 @@ def MG():
 def heungkuk():
     st.title("흥국화재")
     st.subheader("대표번호")
-    st.text(number(8))
-    st.subheader("상담센터")
-    st.link_button(consult(8), consult(8))
-    st.subheader("민원창구")
-    st.link_button(complain(8), complain(8))
+    st.text(number(4))
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(4))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(4))
+    st.subheader("특약 종류")
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"흥국화재_{i} 페이지"))
 
-# 보험사별: 하나손해보험
-def hana():
-    st.title("하나손해보험")
-    st.subheader("대표번호")
-    st.text(number(10))
-    st.subheader("상담센터")
-    st.link_button(consult(10), consult(10))
-    st.subheader("민원창구")
-    st.link_button(complain(10), complain(10))
-    for i in df['구분'].unique():
-        st.button(i, on_click = lambda i=i: go_to(f"하나손해보험_{i} 페이지"))
+# # 보험사별: 하나손해보험
+# def hana():
+#     st.title("하나손해보험")
+#     st.subheader("대표번호")
+#     st.text(number(10))
+#     st.subheader("상담센터")
+#     st.link_button(consult(10), consult(10))
+#     st.subheader("민원창구")
+#     st.link_button(complain(10), complain(10))
+#     for i in df['구분'].unique():
+#         st.button(i, on_click = lambda i=i: go_to(f"하나손해보험_{i} 페이지"))
 
 # 보험사별: 캐롯손해보험
 def carrot():
     st.title("캐롯손해보험")
     st.subheader("대표번호")
     st.text(number(11))
-    st.subheader("상담센터")
-    st.link_button(consult(11), consult(11))
-    st.subheader("민원창구")
-    st.link_button(complain(11), complain(11))
+    st.subheader("상담센터 및 민원창구 바로가기")
+    left_col, right_col = st.columns(2)
+    with left_col:
+        st.link_button("상담센터 바로가기", consult(11))
+    with right_col:
+        st.link_button("민원창구 바로가기", complain(11))
+    st.subheader("특약 종류")
     for i in df['구분'].unique():
         st.button(i, on_click = lambda i=i: go_to(f"캐롯손해보험_{i} 페이지"))
 
@@ -255,7 +290,6 @@ def dynamic_detail_page():
     st.title(f"{company} - {category}")
 
         # 필터링된 DataFrame 가져오기
-    # filtered_df = df[['회사명', '구분']]
     
     filtered_df = df[(df["회사명"] == company) & (df["구분"] == category)]
     with st.expander("✅ 세부 정보 보기"):
@@ -267,9 +301,13 @@ def dynamic_detail_page():
             st.markdown(f"**가입조건**: {row.get('가입조건', '-')}")
             if "비고" in row:
                 st.markdown(f"**비고**: {row['비고']}")
+    st.dataframe(filtered_df)
 
 
-
+# 뒤로가기는 홈화면에선 보여주지 마세요.
+if st.session_state.current_page != "home":
+    st.button("⬅", on_click=go_back)
+    
 # 딕셔너리에 페이지와 함수 매핑
 pages = {
     "home":home,
@@ -285,7 +323,7 @@ pages = {
     "롯데손보 페이지": lotte,
     "MG손보 페이지":   MG,
     "흥국화재 페이지": heungkuk,
-    "하나손해보험 페이지": hana,
+    # "하나손해보험 페이지": hana,
     "캐롯손해보험 페이지": carrot
 }
 
@@ -295,12 +333,7 @@ if st.session_state.current_page in pages:
 else:
     dynamic_detail_page()
 
-# pages라는 딕셔너리에서 key로 value 불러오기 -> 함수 호출
-# pages[st.session_state.current_page]()
 
-# 뒤로가기는 홈화면에선 보여주지 마세요.
-if st.session_state.current_page != "home":
-    st.button("⬅", on_click=go_back)
 
 
 # 그냥 커스텀어트리뷰트 current_page랑 history 실시간 조회.
